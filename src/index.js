e7c010f71cdb6b82e9d219bb802751d5700e3315
Suraj Jha <44930179+jhasuraj01@users.noreply.github.com>
2020-07-19 00:35:12 +0530

    Refractored Code: Used Promise

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ const scrapper = require("./scraper");
 
 let url = "https://stackoverflow.com/questions/23326561/get-title-of-a-page-with-cheerio";
 
-let a = scrapper(url);
-console.log(a);
+const start = async () => {
+    let a = await scrapper(url);
+    console.log(a);
+}
 
-
+start();
--- src/scraper.js
+++ src/scraper.js
@@ -6,50 +6,49 @@ const path = "./index.html";
 const cheerio = require('cheerio');
 const writeStream = fs.createWriteStream(path, "utf-8");
 let data = {
-    "time": new Date().getTime()
+    time: Date.now()
 };
 
 // actual scrape function
 const scrap = (url) => {
+    return new Promise((resolve, reject) => {
+        // request function
+        const req = https.request(url, (res) => {
+            // get status code
+            console.log('statusCode:', res.statusCode);
 
-    // request function
-    const req = https.request(url, (res) => {
-        // get status code
-        console.log('statusCode:', res.statusCode);
+            // writing the file
+            res.pipe(writeStream);
 
-        // writing the file
-        res.pipe(writeStream);
+            //once it has been written reading it
+            writeStream.on('finish', () => {
 
-        //once it has been written reading it
-        writeStream.on('finish', () => {
+                let res = fs.readFileSync(path, "utf-8");
+                // console.log(readStream);
 
-            let res = fs.readFileSync(path, "utf-8");
-            // console.log(readStream);
+                // use html parser to get meta tags value
+                let $ = cheerio.load(res.toString());
+                data.title = $('meta[property="og:title"]').attr('content') || $('title').text() || "No title available";
+                data.description = $('meta[property="og:description"]').attr('content') || "No description available";
+                data.image = $('meta[property="og:image"]').attr('content') || "No image available";
+                data.url = $('meta[property="og:url"]').attr('content') || "No title available";
 
-            // use html parser to get meta tags value
-            let $ = cheerio.load(res.toString());
-            data.title = $('meta[property="og:title"]').attr('content') || $('title').text() || "No title available";
-            data.description = $('meta[property="og:description"]').attr('content') || "No description available";
-            data.image = $('meta[property="og:image"]').attr('content') || "No image available";
-            data.url = $('meta[property="og:url"]').attr('content') || "No title available";
+                // deleting after we have read the file
+                fs.unlinkSync(path);
 
-            // deleting after we have read the file
-            fs.unlinkSync(path);
+                resolve(data);
 
-            // return the json file
-
-            // console.log(data);
+            });
 
         });
 
-    });
+        req.on('error', (e) => {
+            console.error(e);
+            reject(e);
+        });
+        req.end();
+    })
 
-    req.on('error', (e) => {
-        console.error(e);
-    });
-    req.end();
-
-    return data;
 }
 
 module.exports = scrap;
