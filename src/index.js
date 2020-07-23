4781712df255d95d3b71a5d8790956627ad3520e
jhamadhav <jhamadhav28@gmail.com>
2020-07-23 19:56:31 +0530

    cors added for api functions

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 // npm modules
 const express = require("express");
+const cors = require('cors')
 const bodyParser = require("body-parser");
 const app = express();
 
@@ -13,6 +14,7 @@ const { create_new, findByUrl } = require("./myDB");
 const port = process.env.PORT || 3000;
 
 // parser
+app.use(cors());
 app.use("/", bodyParser.urlencoded({ extended: false }));
 app.use(express.static("public"));
 app.use(express.json());
