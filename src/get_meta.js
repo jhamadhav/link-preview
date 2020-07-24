0cad0771fed78fabe57ff916dfb69143b0fea762
jhamadhav <jhamadhav28@gmail.com>
2020-07-25 00:42:31 +0530

    title and description length limited to standards

--- src/get_meta.js
+++ src/get_meta.js
@@ -8,7 +8,18 @@ const meta_info = (doc, url0) => {
     // values for the meta tags
     let title = $('meta[property="og:title"]').attr('content') || $('title').text() || undefined;
 
+    // limiting the length 
+    if (title.length > 70) {
+        title = title.substring(0, 70) + "...";
+    } title.trim();
+
+
     let description = $('meta[property="og:description"]').attr('content') || $('p').first().text() || undefined;
+
+    // limiting the length 
+    if (description.length > 200) {
+        description = description.substring(0, 200) + "...";
+    } description.trim();
 
     let image = $('meta[property="og:image"]').attr('content') || $('link[re="icon"]').attr('href') || $('img').first().attr('src') || undefined;
     // some conditions for the image url
@@ -18,7 +29,6 @@ const meta_info = (doc, url0) => {
             image = url0 + image;
         }
     }
-    let url = $('meta[property="og:url"]').attr('content') || url0;
 
     // constructing the actual json to send
     let msec_in_7days = 7 * 24 * 60 * 60 * 1000;
