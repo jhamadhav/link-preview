0cfb3f8f86f13fbf232e32da68b3ba4a3c7c42f8
jhamadhav <jhamadhav28@gmail.com>
2020-07-27 20:06:12 +0530

    minor patch

--- src/get_meta.js
+++ src/get_meta.js
@@ -1,9 +1,10 @@
 // parser
 const cheerio = require('cheerio');
+let $;
 
 const meta_info = (doc, url0) => {
     // use html parser to get meta tags value
-    let $ = cheerio.load(doc);
+    $ = cheerio.load(doc);
 
     // values for the meta tags
     let title = $('meta[property="og:title"]').attr('content') || $('title').text() || undefined;
@@ -11,15 +12,17 @@ const meta_info = (doc, url0) => {
     // limiting the length 
     if (title.length > 70) {
         title = title.substring(0, 70) + "...";
-    } title.trim();
+    }
+    title = title.trim();
 
 
-    let description = $('meta[property="og:description"]').attr('content') || $('p').first().text() || undefined;
+    let description = $('meta[property="og:description"]').attr('content') || get_from("p") || get_from("div") || undefined;
 
     // limiting the length 
     if (description.length > 200) {
         description = description.substring(0, 200) + "...";
-    } description.trim();
+    }
+    description = description.trim();
 
     let image = $('meta[property="og:image"]').attr('content') || $('link[re="icon"]').attr('href') || $('img').first().attr('src') || undefined;
     // some conditions for the image url
@@ -31,7 +34,7 @@ const meta_info = (doc, url0) => {
     }
 
     // constructing the actual json to send
-    let msec_in_7days = 7 * 24 * 60 * 60 * 1000;
+    let msec_in_7days = 1000 * 60 * 60 * 24 * 7;
     let data = {
         "time": Date.now() + msec_in_7days,
         "title": title,
@@ -40,6 +43,19 @@ const meta_info = (doc, url0) => {
         "url": url0
     };
     return data;
+}
+
+// get data from p tags
+const get_from = (tag = "p") => {
+    let res = undefined;
+    for (let i = 1; i < 5; i++) {
+        res = $(`${tag}:nth-of-type(${i})`).text();
+        res = res.trim();
+        if (res !== undefined && res.length > 1) {
+            return res;
+        }
+    }
+    return undefined;
 }
 
 module.exports = meta_info;
